827c03c05cf4024846b794bc125c15035bdfda9b
Ramahadi <rmoheane@oldmutual.com>
2024-02-07 09:14:15 +0200

    initial commit

---
+++ src/main/kotlin/com/example/demo1/Demo1Application.kt
@@ -0,0 +1,10 @@
+package com.example.demo1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo1Application
+
+fun main(>) {
+print("hello")}
